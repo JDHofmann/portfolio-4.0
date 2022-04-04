d4a34065d41b14c8bc2a4e706a35a35d7b21f854
jdhofmann <justindhofmann@gmail.com>
2022-04-04 13:38:51 -0400

    iframe styling and added display ads

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -6,28 +6,30 @@
 		</div>
 		<p class="description">{{ project.description }}</p>
 
-		<section>
+		<section v-if="!project.iframes">
 			<h2>Images</h2>
-			<div id="project-images">
+			<div id="project-images" class="images">
 				<div v-for="(img, i) in project.images" :key="i">
 					<h3>{{ img.title }}</h3>
 					<img :src="img.path" :alt="img.title" />
 				</div>
 			</div>
 		</section>
-		<!-- <nuxt-img
-			provider="cloudinary"
-			src="/21D30085_Multichannel_DA_Flo-Balloons-300x250/21D30085_Multichannel_DA_Flo-Balloons-300x250.jpg"
-			alt=""
-		/> -->
-		<iframe
-			v-if="project.iframes"
-			:src="project.iframes[0].path"
-			frameborder="0"
-			:width="project.iframes[0].width"
-			:height="project.iframes[0].height"
-		></iframe>
-		<section class="links">
+		<section v-if="project.iframes">
+			<h2>Animated Ads</h2>
+			<div id="project-images">
+				<div v-for="(iframe, i) in project.iframes" :key="i">
+					<h3>{{ iframe.title }}</h3>
+					<iframe
+						:src="iframe.path"
+						frameborder="0"
+						:width="iframe.width"
+						:height="iframe.height"
+					></iframe>
+				</div>
+			</div>
+		</section>
+		<section class="links" v-if="project.links">
 			<h2>Links</h2>
 			<ul>
 				<li v-for="(link, i) in project.links" :key="i">
@@ -60,25 +62,38 @@ export default {
 		padding: 0.25rem 0 1rem;
 	}
 }
+// #project-images {
+// 	display: flex;
+// 	justify-content: space-evenly;
+// 	flex-wrap: wrap;
+// 	display: grid;
+// 	grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+// 	// align-items: end;
+// 	justify-items: center;
+// 	grid-column-gap: 1rem;
+// 	grid-row-gap: 1rem;
+// 	width: 100%;
+// }
+#project-iframes,
 #project-images {
 	display: flex;
-	justify-content: space-evenly;
 	flex-wrap: wrap;
-	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
-	align-items: end;
-	justify-items: center;
-	grid-column-gap: 1rem;
-	grid-row-gap: 1rem;
-	width: 100%;
+	justify-content: space-evenly;
+	gap: 1rem;
 }
 #project-images div {
-	width: 80%;
 	padding: 1rem 2rem 2rem;
+}
+#project-images.images div {
+	width: 80%;
+	max-width: 400px;
 }
 #project-images img {
 	width: 100%;
 	border: 1px solid;
+}
+iframe {
+	background: #ffffff;
 }
 h3 {
 	padding: 0.5rem 0;
